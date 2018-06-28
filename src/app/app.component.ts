b3ea1f1f18fec1009230903f7d0d79b292c9db05
Rohan Sonawane <rohansonawane555@gmail.com>
2018-06-28 19:16:03 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  private gridApi;
+  private gridColumnApi;
+  private columnDefs;
+  private sortingOrder;
+
+  constructor(private http:HttpClient){
+    this.columnDefs=[
+      {
+        headerName:"Athlete",
+        field:"athlete",
+        width: 150,
+        sortingOrder:["asc","desc"]
+      },
+      {
+        headerName:"Age",
+        field:"age",
+        width:90,
+        sortingOrder:["desc","asc"]
+      },
+      {
+        headerName:"Country",
+        field:"country",
+        width:120,
+        sortingOrder:["asc","desc"]
+      },
+      {
+        headerName:"Year",
+        field:"year",
+        width:90,
+        sortingOrder:["asc","desc"]
+      }
+    ];
+    this.sortingOrder=["desc","asc",null];
+  }
+
+  onGridReady(params){
+    this.gridApi=params.api;
+    this.gridColumnApi=params.columnApi;
+    this.http.get("https://raw.githubusercontent.com/ag-grid/ag-grid-docs/master/src/olympicWinnersSmall.json")
+    .subscribe(data=>{
+      params.api.setRowData(data);
+    })
+    
+  }
 }
